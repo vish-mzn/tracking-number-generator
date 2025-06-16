807ec4d2daee91007c033e1bf2028390107034d2
vpahuja <vishesh.phj@silverlakeaxis.com>
2025-06-16 23:08:49 +0800

    initializing using spring initilizr

---
+++ src/main/java/com/project/numbergenerator/TrackingNumberGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.project.numbergenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingNumberGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingNumberGeneratorApplication.class, args);
+	}
+
+}
